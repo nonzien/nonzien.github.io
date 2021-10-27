f47a85004b2b12a2f8daf2e1213210320accc8b0
Nonso Emmanuel <nwanukposiemmanuel@gmail.com>
2021-10-28 00:40:52 +0100

    Update toggle.js
    uodate

--- Javascript/toggle.js
+++ Javascript/toggle.js
@@ -1,9 +1,22 @@
 function dropdownMenu() {
-    var x = document.getElementById("dropdownClick");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-        // change topnav to topnav.responsive
-    } else {
-        x.className = "topnav";
+        var x = document.getElementById("dropdownClick");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+            // change topnav to topnav.responsive
+        } else {
+            x.className = "topnav";
+        }
     }
-}
+
+    const portfolioItems = document.querySelectorAll('.row-3');
+     
+        portfolioItems.forEach(portfolioitem => {
+         portfolioitem.addEventListener('mouseover', () => {
+           console.log(portfolioitem.childNodes[1].classList)
+           portfolioitem.childNodes[1].classList.add('image-darken');
+         });
+     
+         portfolioitem.addEventListener('mouseout', () => {
+           portfolioitem.childNodes[1].classList.remove('image-darken');
+         });
+       });
